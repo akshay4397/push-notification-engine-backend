11e6c76c07fe8f21e196528d2bf41f426ebf8eae
akshay patil <akshay.patil@coreviewsystems.com>
2020-07-12 18:54:42 +0530

    SP-2: added controller and stubs for web-socket api

---
+++ src/main/java/com/pushnotification/engine/controller/WebSocketNotificationController.java
@@ -0,0 +1,24 @@
+package com.pushnotification.engine.controller;
+
+import com.pushnotification.engine.dto.Response;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Controller
+public class WebSocketNotificationController {
+
+    @MessageMapping("/get_notifications")
+    @SendTo("/topic/new_notifications")
+    public Response getNotifications() {
+        return new Response("Test response");
+    }
+
+    @ExceptionHandler
+    @SendTo("/topic/errors")
+    public Response exceptionHandler() {
+        return new Response("Error processing request");
+    }
+
+}
