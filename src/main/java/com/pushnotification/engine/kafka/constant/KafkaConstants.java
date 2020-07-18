e068c690358e765a202546ec15d027e990fcdd5f
akshay patil <akshay.patil@coreviewsystems.com>
2020-07-18 18:13:39 +0530

    updated docker deployment files

--- src/main/java/com/pushnotification/engine/kafka/constant/KafkaConstants.java
+++ src/main/java/com/pushnotification/engine/kafka/constant/KafkaConstants.java
@@ -2,7 +2,7 @@ package com.pushnotification.engine.kafka.constant;
 
 public class KafkaConstants {
 
-    public static final String BOOTSTRAP_SERVER = "13.233.121.236:9092";
+    public static final String BOOTSTRAP_SERVER = "13.234.19.202:9092";
     public static final String NOTIFICATION_TOPIC = "notifications";
     public static final String CONSUMER_GROUP_ID = "new-notifications-consumer";
 }
